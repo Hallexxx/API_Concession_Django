41a0c749546952aa879588fddc7d0c79d606893e
Alexandre Perez <112977841+Hallexxx@users.noreply.github.com>
2024-12-16 16:01:21 +0100

    ajout filtre

--- concession_project/api/views.py
+++ concession_project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Concession, Vehicule
 from .serializers import ConcessionReadSerializer, ConcessionWriteSerializer, VehiculeSerializer
 
@@ -13,3 +14,14 @@ class ConcessionViewSet(viewsets.ModelViewSet):
 class VehiculeViewSet(viewsets.ModelViewSet):
     queryset = Vehicule.objects.all()
     serializer_class = VehiculeSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['marque', 'date_mise_en_service', 'concession']
+
+    def get_queryset(self):
+        """
+        Filtrer les véhicules en fonction de la concession si elle est spécifiée dans l'URL.
+        """
+        concession_id = self.kwargs.get('concession_pk')
+        if concession_id:
+            return Vehicule.objects.filter(concession_id=concession_id)
+        return super().get_queryset()
